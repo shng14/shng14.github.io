1b594f2e4360c04a8859d7ee54ee2471ec83df73
Nhi Nguyễn <mt-qc20@nidec-copal.vn>
2019-05-07 13:40:52 +0700

    up

--- js/App.js
+++ js/App.js
@@ -1,26 +1,92 @@
 ﻿function cal(num1, num2) {
     var res;
-    if (document.getElementById("callist").value == "cong") {
-        res = parseInt(num1) + parseInt(num2);
-    }
-    else if (document.getElementById("callist").value == "tru") {
-        res = num1 - num2;
-    }
-    else if (document.getElementById("callist").value == "nhan") {
-        res = num1 * num2;
-    }
-    else if (document.getElementById("callist").value == "chia") {
-        if (num2 != 0)
+    if (num1 != "" && num2 != "")
+    {
+        if (document.getElementById("callist").value == "cong")
         {
-            res = num1 / num2;
+            res = parseInt(num1) + parseInt(num2);
         }
-        else
+        else if (document.getElementById("callist").value == "tru")
         {
-            swal("The Second number cannot be 0!");
-            return;
+            res = num1 - num2;
         }
+        else if (document.getElementById("callist").value == "nhan")
+        {
+            res = num1 * num2;
+        }
+        else if (document.getElementById("callist").value == "chia")
+        {
+            if (num2 != 0)
+            {
+                res = num1 / num2;
+            }
+            else
+            {
+                swal("The Second number cannot be 0!", "", "warning");
+                return;
+            }
+        }
+    }
+    else
+    {
+        swal("The numbers is invalid!", "", "warning");
+        return;
     }
     document.getElementById("result").innerHTML = res;
+}
+
+function ketqua() {
+    var res1 = false;
+    var res2 = false;
+    var res3 = false;
+    var point = 0;
+    var s_one;
+    var s_two;
+    var s_three;
+    var cau1 = document.getElementsByName("s1");
+    var cau2 = document.getElementsByName("s2");
+    var cau3 = document.getElementsByName("s3");
+    for (var i = 0; i < cau1.length; i++)
+    {
+        if (cau1[i].checked == true)
+        {
+            s_one = cau1[i].value;
+        }
+        if (cau2[i].checked == true)
+        {
+            s_two = cau2[i].value;
+        }
+        if (cau3[i].checked == true)
+        {
+            s_three = cau3[i].value;
+        }
+    }
+
+    if (s_one == 15)
+    {
+        //res1 = true;
+        point += 20;
+    }
+    if (s_two == 24)
+    {
+        //res2 = true;
+        point += 40;
+    }
+    if (s_three == 2400)
+    {
+        //res3 = true;
+        point += 40;
+    }
+    document.getElementById("point").innerHTML = point;
+}
+function demo()
+{
+    var checkbox = document.getElementsByName("s2");
+    for (var i = 0; i < checkbox.length; i++){
+        if (checkbox[i].checked === true){
+            alert(checkbox[i].value);
+        }
+    }
 }
 function mensaje() {
 
