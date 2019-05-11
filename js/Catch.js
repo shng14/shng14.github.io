4bdc2431f1f4d65ebba8515115047de64584f32b
Nhi Nguyễn <mt-qc20@nidec-copal.vn>
2019-05-11 14:05:20 +0700

    up

--- js/Catch.js
+++ js/Catch.js
@@ -9,60 +9,54 @@ window.onclick = function (event) {
 }
 
 function demo(usr, pwd) {
-    if (usr == "admin" && pwd == "admin")
-    {
-        return true;
+    var data = '[ { "user": "admin", "pwd": "admin" }, { "user": "nhi", "pwd": "nhi" }, { "user": "user1", "pwd": "1111" } ]';
+    data = JSON.parse(data);
+    //console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        //console.log(data[i].user);
+        //alert("hello");
+        if (usr == data[i].user && pwd == data[i].pwd) {
+            //console.log(data[i].user);
+            return true;
+        }
     }
-    else
-    {
-        swal("Username and Password is invalid!", "", "warning");
-        //document.getElementById('warn').innerHTML = "* Username and Password is invalid";
-        return false;
-    }
+    swal("Username and Password is invalid!", "", "warning");
+    //document.getElementById('warn').innerHTML = "* Username and Password is invalid";
+    return false;
 }
 
-function calculate(hei, wei)
-{
-    if (hei != "" && wei != "")
-    {
+function calculate(hei, wei) {
+    if (hei != "" && wei != "") {
         if (hei > 2) { swal("Chiều cao không được vượt quá 2m", "", "warning"); document.getElementById('txtHeight').focus(); return; }
-        
+
         var bmi = wei / (hei * hei);
         document.getElementById('yBMI').innerHTML = bmi;
-        if (bmi < 18.5)
-        {
+        if (bmi < 18.5) {
             document.getElementById('kind').innerHTML = "Thiếu cân"
         }
-        else if (bmi >= 18.5 && bmi < 23)
-        {
+        else if (bmi >= 18.5 && bmi < 23) {
             document.getElementById('kind').innerHTML = "Bình thường"
         }
-        else if (bmi >= 23 && bmi < 25)
-        {
+        else if (bmi >= 23 && bmi < 25) {
             document.getElementById('kind').innerHTML = "Thừa cân"
         }
-        else if (bmi >= 25 && bmi < 30)
-        {
+        else if (bmi >= 25 && bmi < 30) {
             document.getElementById('kind').innerHTML = "Béo phì độ I"
         }
-        else if (bmi >= 30 && bmi < 40)
-        {
+        else if (bmi >= 30 && bmi < 40) {
             document.getElementById('kind').innerHTML = "Béo phì độ II"
         }
-        else
-        {
+        else {
             document.getElementById('kind').innerHTML = "Béo phì độ III"
         }
         document.getElementById('err').innerHTML = "";
     }
-    else if (hei != "" && wei == "")
-    {
+    else if (hei != "" && wei == "") {
         swal("Cân nặng không hợp lệ!", "", "warning");
         //document.getElementById('err').innerHTML = "* Chiều cao không hợp lệ";
         document.getElementById('txtWeight').focus();
     }
-    else if (hei == "" && wei != "")
-    {
+    else if (hei == "" && wei != "") {
         swal("Chiều cao không hợp lệ!", "", "warning");
         //document.getElementById('err').innerHTML = "* Cân nặng không hợp lệ";
         document.getElementById('txtHeight').focus();
